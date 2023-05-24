e9cfb2bb2088f42464dfe328efbdf40ffcf5be5a
saagarbhujel <saagarbhujel22@gmail.com>
2023-05-24 11:24:06 +0545

    events handeling done

--- src/Components/auth/SignUp.jsx
+++ src/Components/auth/SignUp.jsx
@@ -6,24 +6,41 @@ const [name, setName] = useState('')
 const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 
+const handleName=(e)=>{
+  setName(e.target.value)
+}
+
+const handleEmail =(e)=>{
+  setEmail(e.target.value)
+}
+
+const handlePassword=(e)=>{
+  setPassword(e.target.value)
+}
+
+const submitFrom=(e)=>{
+  e.preventDefault();
+  console.log(name,email,password)
+}
+
   return (
     <>
   <form>
   <div className="container">
     <div className="userName">
         <p>Enter Your name</p>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" onChange={handleName} value={name}/>
     </div>
     <div className="email">
         <p>Enter Your email</p>
-        <input type="email" name="email" id="email" />
+        <input type="email" onChange={handleEmail} value={email} />
     </div>
     <div className="password">
         <p>Enter your password</p>
-        <input type="password" name="password" id="pswrd" />
+        <input type="password"  onChange={handlePassword} value={password}/>
     </div>
     <div className="btn">
-     <button>Signup</button>
+     <button className='btn' onClick={submitFrom}>Signup</button>
     </div>
    </div>
   </form>
